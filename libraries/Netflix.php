5c6236a5331a56ad127340ca0b9aca9f5da7cc71
Jim <jimdoescode@gmail.com>
2012-02-13 17:05:02 -0500

    Updated library files to comply with CodeIgniter standards

--- libraries/Netflix.php
+++ libraries/Netflix.php
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-class netflix
+class Netflix
 {
     const SCHEME        = 'http';
     const HOST          = 'api.netflix.com';
